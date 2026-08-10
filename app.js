53ff2b76f8b7fcbfc99595e5d50647cbfde7159b
Abhishek Sunda <abhishek@Abhisheks-MacBook-Air.local>
2026-08-10 17:01:40 +0530

    Task 2 pushed.

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
 'use strict';
 
 // Storefront and experiment behaviour will be added task by task.
+
+function createPromoCard(){
+    const promoCard = document.querySelector('[wingify-promo-card]');
+    if(promoCard)return promoCard;
+
+
+    
+    const newCard = document.createElement('article');
+    newCard.setAttribute('id', "nova-promo-card");
+    newCard.classList.add('promo-card', 'product-card');
+
+    const header = document.createElement('h3');
+    header.textContent = "Summer Special";
+
+    const p = document.createElement('p');
+    p.textContent = "Get 20% off selected products."
+
+    const button = document.createElement('button');
+
+    button.textContent = "Explore Offer";
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', ()=>{
+        console.log("promotional button clicked!");
+    })
+
+    newCard.append(header, p, button);
+
+    return newCard;
+
+}
+
+function insertElement(){
+    const activePromoCard = document.querySelector('#nova-promo-card');
+    if(activePromoCard)return;
+
+    const newPromoCard = createPromoCard();
+
+    const grid = document.querySelector('#product-grid');
+    if(!grid)return;
+    const secondProductCard = grid.querySelector('[data-product-id="product-2"]');
+    if(!secondProductCard)return;
+    secondProductCard.after(newPromoCard);
+
+
+}
+
+function activateExperiment(){
+    const productGrid = document.querySelector('#product-grid');
+    if(!productGrid){
+        document.addEventListener('DOMContentLoaded', insertElement, {once:true});
+        return;
+    }
+
+    insertElement();
+}
+
+function deactivateExperiment(){
+    const activePromoCard = document.querySelector('#nova-promo-card');
+    if(activePromoCard)activePromoCard.remove();
+}
+
+activateExperiment();
